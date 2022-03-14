36ea79d3c5e4212a5b60aa4751b6f6115a304efe
Raúl Acosta Herazo <r.acosta.herazo@gmail.com>
2022-03-13 19:35:54 -0500

    Finishes exercise

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  i <- NULL
+  set <- function( matrix ) {
+    m <<- matrix
+    i <<- NULL
+  }
+  
+  get <- function() {
+    m
+  }
+  
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  getInverse <- function() {
+    i
+  }
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
@@ -11,5 +33,21 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  
+  if( !is.null(m) ) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  
+  m <- solve(data) %*% data
+  
+  x$setInverse(m)
+  
+  m
+  
 }
